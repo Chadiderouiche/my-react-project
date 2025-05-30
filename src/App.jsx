c332e15df5ab0867541da5a857b961caf1ec86fb
Chadi Derouiche <chadiderouiche@Chadis-MacBook-Air.local>
2025-05-30 16:50:30 +0100

    W4 React Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,78 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function getTitle(title){
+  return title;
+} 
+const App = () =>{
+  const stories = [
+  {
+    title:"React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title:"Redux",
+    url: "https://reduxjs.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1, 
+  },
+]
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+return(
+    <div>
+      <h1>My hacker stories</h1>
+      <search />
+      <hr />
+      <list list={stories}/>
+    </div>
+);
+ } ;
+
+const Search = () => {
+  const [searchTerm,setSearchTerm] = React.useState('');
+  const handlechange =(Event)=>{
+    setSearchTerm(Event.target.value);
+    console.log(Event);
+    // value of target (here:input HTML element)
+    console.log(Event.target.value);
+
+  };
+return(
+    <div>     
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onchange={handlechange} />
+      <p>
+        Searching for  <strong>{searchTerm}</strong>
       </p>
-    </>
-  )
-}
 
-export default App
+    </div>
+);
+};
+const List = (props) => (
+    
+      <ul>
+        {props.list.map((item) => (
+          <item key={item.objectifID} item={item} />
+
+        ))}
+      </ul>
+);
+const item=(props)=>(
+ <li>
+    <span>
+      <a herf={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
+);
+
+
+export default App;
