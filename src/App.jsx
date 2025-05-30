29be00d2f96bb4f4cf57917a8703d1cdb59c61d7
Chadi Derouiche <chadiderouiche@Chadis-MacBook-Air.local>
2025-05-30 20:21:49 +0100

    W6 Filtering the list

--- src/App.jsx
+++ src/App.jsx
@@ -5,67 +5,85 @@ import './App.css'
 
 import * as React from "react";
 
-// List of items
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  console.log("App renders");
 
-// 1. Refactor App component to arrow function
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
-    <Search />
-    <hr />
-    <List />
-  </div>
-);
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-// 4. Refactor Search to block body (with logic inside)
-// 5. Add handler function for the input
-const Search = () => {
-  const handleChange = (event) => {
-    console.log(event.target.value);
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
   };
+
+  const filteredStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
+      <h1>My Hacker Stories</h1>
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
+      <hr />
+      <List list={filteredStories} />
     </div>
   );
 };
 
-// 2. Refactor callback in map() to arrow function
-// 3. Refactor List to arrow function and concise body
-const List = () => (
-  <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>{" "}
-        <span>{item.author}</span>{" "}
-        <span>{item.num_comments}</span>{" "}
-        <span>{item.points}</span>
-      </li>
-    ))}
-  </ul>
-);
+const Search = ({ searchTerm, onSearch }) => {
+  console.log("Search renders");
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={searchTerm} onChange={onSearch} />
+      <p>Searching for: <strong>{searchTerm}</strong></p>
+    </div>
+  );
+};
+
+const List = ({ list }) => {
+  console.log("List renders");
+  return (
+    <ul>
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const Item = ({ item }) => {
+  console.log("Item renders");
+  return (
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>{" "}
+      <span>{item.author}</span>{" "}
+      <span>{item.num_comments}</span>{" "}
+      <span>{item.points}</span>
+    </li>
+  );
+};
 
 export default App;
+
+
 
